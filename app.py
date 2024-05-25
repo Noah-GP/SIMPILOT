b1c4baaf03fa20a0c3053051589ca21ac0e6ccb6
Noah-GP <161973719+Noah-GP@users.noreply.github.com>
2024-05-25 18:20:39 +0200

    newesest updates

--- app.py
+++ app.py
@@ -10,5 +10,21 @@ def home():
 def news():
     return render_template('news.html')
 
+@app.route('/checklist')
+def checklist():
+    return render_template('checklist.html')
+
+@app.route('/forum')
+def forum():
+    return render_template('forum.html')
+
+@app.route('/events')
+def events():
+    return render_template('events.html')
+
+@app.route('/add_on')
+def add_on():
+    return render_template('add_on.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
